fb63201ef7e8d53b9b4919860a999014962cdacb
sandra2p <71703693+sandra2p@users.noreply.github.com>
2022-02-18 11:48:58 +0100

    Commit to upload

---
+++ spring-boot-jsp-example/src/test/java/net/viralpatel/springbootjspexample/SpringBootJspExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.viralpatel.springbootjspexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
